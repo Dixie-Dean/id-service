430ba97a25fc8ccb36a150021d50055c615c51a4
Dmitry Sokolov <d.w.sokolov26@gmail.com>
2025-03-18 15:40:39 +0300

    Cache (#3)
    * feature: add id batch
    
    * chore: add synchronized batch size check (bottleneck)
    
    * chore: make batch replenishment asynchronous

--- src/main/java/dev/dixie/service/UniqueIdGeneratorService.java
+++ src/main/java/dev/dixie/service/UniqueIdGeneratorService.java
@@ -5,36 +5,92 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import java.util.Base64;
-import java.util.Random;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UniqueIdGeneratorService implements IdService {
 
-    private static final int BYTE_LENGTH = 6;
-    private final static String LISTEN_TO_TOPIC_NAME = "request-id-topic";
-    private final static String SEND_TO_TOPIC_NAME = "provide-id-topic";
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newSingleThreadExecutor();
+    private static final JedisPool JEDIS_POOL = new JedisPool("localhost", 6381);
+    private static final AtomicLong KEY_COUNTER = new AtomicLong();
+    private static final int MIN_BATCH_SIZE = 5;
+    private static final String LISTEN_TO_TOPIC_NAME = "request-id-topic";
+    private static final String SEND_TO_TOPIC_NAME = "provide-id-topic";
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Override
-    public String generateId() {
-        byte[] randomBytes = new byte[BYTE_LENGTH];
-        new Random().nextBytes(randomBytes);
-        String id = Base64.getUrlEncoder().encodeToString(randomBytes);
+    public String generateID() {
+        var uuidBytes = UUID.randomUUID().toString().getBytes();
+        var encodedID = Base64.getUrlEncoder().encodeToString(uuidBytes);
+        var id = encodedID.substring(0, 8);
         log.info("GenerateID | ID:{}", id);
         return id;
     }
 
+    private String retrieveID() {
+        try (var jedis = JEDIS_POOL.getResource()) {
+            if (isBatchSizeLessThanMin(jedis)) {
+                replenishBatchAsync();
+            }
+            var key = getRandomKey(jedis).orElseGet(this::generateID);
+            log.info("retrieveID | {}", key);
+            return jedis.getDel(key);
+        }
+    }
+
+    private Optional<String> getRandomKey(Jedis jedis) {
+        return Optional.ofNullable(jedis.randomKey());
+    }
+
+    private boolean isBatchSizeLessThanMin(Jedis jedis) {
+        var checkResult = jedis.dbSize() < MIN_BATCH_SIZE;
+        log.info("isBatchSizeLessThanMin | result:{}", checkResult);
+        return jedis.dbSize() < MIN_BATCH_SIZE;
+    }
+
+    private void replenishBatchAsync() {
+        log.info("replenishBatchAsync | called");
+        CompletableFuture.runAsync(() -> {
+            try (var jedis = JEDIS_POOL.getResource()) {
+                if (isBatchSizeLessThanMin(jedis)) {
+                    replenishBatch(jedis);
+                }
+            }
+        }, EXECUTOR_SERVICE)
+                .exceptionally(exception -> {
+                    log.error("replenishBatchAsync | exception:{}", exception.getMessage());
+                    return null;
+                });
+    }
+
+    private void replenishBatch(Jedis jedis) {
+        for (int i = 0; i < MIN_BATCH_SIZE * 2; i++) {
+            pushID(jedis);
+        }
+    }
+
+    private void pushID(Jedis jedis) {
+        jedis.set("key:%d".formatted(KEY_COUNTER.getAndIncrement()), generateID());
+    }
+
     @KafkaListener(topics = LISTEN_TO_TOPIC_NAME, groupId = "imager")
     public void imagerServiceTopicListener(String value) {
-        log.info("ImagerServiceTopicListener | message:{}, topic:{}", value, LISTEN_TO_TOPIC_NAME);
+        log.info("imagerServiceTopicListener | message:{}, topic:{}", value, LISTEN_TO_TOPIC_NAME);
         if ("request-id".equals(value)) {
-            String id = generateId();
+            var id = retrieveID();
             kafkaTemplate.send(SEND_TO_TOPIC_NAME, id);
-            log.info("ImagerServiceTopicListener | ID:{} topic:{}", id, SEND_TO_TOPIC_NAME);
+            log.info("imagerServiceTopicListener | ID:{} topic:{}", id, SEND_TO_TOPIC_NAME);
         }
     }
 }
